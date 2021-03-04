f9e6197fddc17e4dd758457c88f12dabbb76cdee
Simon <bardrabbit@gmail.com>
2021-03-04 01:22:11 -0500

    create app-body and sidebar

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAF78tUeHTpac0_5sK7xhHxxtCr0qHRYUQ',
+  authDomain: 'whats-app-clone-2-acc8c.firebaseapp.com',
+  projectId: 'whats-app-clone-2-acc8c',
+  storageBucket: 'whats-app-clone-2-acc8c.appspot.com',
+  messagingSenderId: '965624800500',
+  appId: '1:965624800500:web:39cbac8cf3537dfb756aa1',
+  measurementId: 'G-8180BBK87X',
+};
